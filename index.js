5ba24f99cdffd27e9d24050e015707304f4e1cf2
Rodrigo <www.rodrigogarcia10@hotmail.com>
2025-03-21 19:20:22 -0300

    instalado eslint no servidor

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const http = require
+const express = require('express');
+
+const app = express();
+
+app.get('/' , function(req, res) {
+    res.send("Ola mundo");
+})
+
+app.listen(3000, function() {
+    console.log("servidor rodadando na porta 3000")
+})
