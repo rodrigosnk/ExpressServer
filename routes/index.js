4ed77b757b3af9618713280fb1641bc23a3f0491
Rodrigo <www.rodrigogarcia10@hotmail.com>
2025-03-22 20:35:55 -0300

    limpando index

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,10 @@
 var express = require('express');
-const mysql = require('mysql2/promise');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  mysql.createConnection({
-    host: 'localhost' ,
-    user : 'adminBackend',
-    password: 'adminadmin',
-    database: 'backend_crud',
-    port: 3306
-  })
-  .then((connection) => {
-      connection.query('SELECT * FROM alunos;')
-      .then((resultado) => {
-          res.send(resultado[0]);
-        });
-      });
-
+ 
+  res.send("ola mundo")
   //res.render('index', { title: 'Trabalho backend' });
 });
 
