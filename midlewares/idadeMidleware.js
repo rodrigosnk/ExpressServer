322339823f886ffd7cf46a2772480c9b072363aa
Rodrigo <www.rodrigogarcia10@hotmail.com>
2025-04-01 18:46:53 -0300

    resolvendo problema de inconsistencia nos midlewares, agora eles so aceitam numeros, OBS pode ser mandado como string

--- midlewares/idadeMidleware.js
+++ midlewares/idadeMidleware.js
@@ -3,8 +3,11 @@ const validacaoIdade = (request , response, next) => {
     if(body.idade == undefined) {
         return response.status(400).json({ message : "O campo idade e obrigatorio" });
     }
-    if(body.idade.trim() === '') {
+    if(body.idade === '') {
         return response.status(400).json({ message : "O campo idade nao pode ser vazio" });
+    }
+    if(Number.isNaN(parseFloat(body.idade))) {
+        return response.status(400).json({message : "O campo idade precisa ser um numero valido"})
     }
     if(parseFloat(body.idade) < 1) {
         return response.status(400).json({ message : "O campo idade tem que ser positivo e maior que 1" });
--- midlewares/precoMidlleware.js
+++ midlewares/precoMidlleware.js
@@ -3,8 +3,11 @@ const validacaoPreco = (request , response, next) => {
     if(body.preco == undefined) {
         return response.status(400).json({ message : "O campo preco e obrigatorio" });
     }
-    if(body.preco.trim() === '') {
+    if(body.preco === '') {
         return response.status(400).json({ message : "O campo preco nao pode ser vazio" });
+    }
+    if(Number.isNaN(parseFloat(body.preco))) {
+        return response.status(400).json({message : "O campo preco precisa ser um numero valido"})
     }
     if(parseFloat(body.preco) < 1) {
         return response.status(400).json({ message : "O campo preco tem que ser positivo e maior que 1" });
